3501b6744759aa9effcc51a28515d40d5f6dc332
EVANDRO RODRIGUES DE ARRUDA <0030482411017@lti.fatecso.edu.br>
2025-11-07 12:27:07 -0300

    Adicição do conteúdo das classes Exame e Medicacao já com multiplicidade

--- src/main/java/fatec/poo/model/Exame.java
+++ src/main/java/fatec/poo/model/Exame.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package fatec.poo.model;
 
 /**
- *
  * @author erixian
  */
 public class Exame {
-    
+
+    private int codigo;
+    private String descricao;
+    private String data;
+    private String horario;
+    private double valor;
+    private Consulta consulta;
+
+    public Exame(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Consulta getConsulta() {
+        return consulta;
+    }
+
+    public void setConsulta(Consulta consulta) {
+        this.consulta = consulta;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
 }
--- src/main/java/fatec/poo/model/Medicacao.java
+++ src/main/java/fatec/poo/model/Medicacao.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package fatec.poo.model;
 
 /**
- *
  * @author erixian
  */
 public class Medicacao {
     
+    private String nome;
+    private String dosagem;
+    private int qtdeDias;
+    private Consulta consulta;
+    
+    /*
+    Na documentação o contrutor da classe Medicacao recebe como parametro
+    uma variavel chamada "descricao", que não existe em nenhuma outra parte
+    da documentação.
+    
+    Devido a modelagem não pedir o metodo "setNome()", nós assumimos que o 
+    parametro de entrada seria "nome" e não "descricao" como sugerido na 
+    modelagem.
+    
+    FALAR COM O DIMAS
+    */
+    
+    public Medicacao(String nome) {
+        this.nome = nome;
+    }
+    
+    public Consulta getConsulta() {
+        return consulta;
+    }
+
+    public void setConsulta(Consulta consulta) {
+        this.consulta = consulta;
+    }
+
+    public String getDosagem() {
+        return dosagem;
+    }
+
+    public void setDosagem(String dosagem) {
+        this.dosagem = dosagem;
+    }
+
+    public int getQtdeDias() {
+        return qtdeDias;
+    }
+
+    public void setQtdeDias(int qtdeDias) {
+        this.qtdeDias = qtdeDias;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }

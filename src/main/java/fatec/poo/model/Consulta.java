42e36cac242484d2fb6e717e989a6b8cb5ed90f0
$rip <cibercianocode@gmail.com>
2025-11-07 12:12:38 -0300

    feat: Classe consulta implementada, assim como a multiplicidade Classe-Exame

--- src/main/java/fatec/poo/model/Consulta.java
+++ src/main/java/fatec/poo/model/Consulta.java
@@ -4,10 +4,53 @@
  */
 package fatec.poo.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author erixian
  */
 public class Consulta {
+    private int codigo;
+    private String data;
+    private double valor;
+    private ArrayList<Exame> exames;
+    private Medico medico;
+    private Paciente Paciente;  
+
+    public Consulta(int codigo, String data) {
+        this.codigo = codigo;
+        this.data = data;
+    }
+    
+    public void addExame(Exame e) {
+        this.exames.add(e);
+        e.setConsulta(this);
+    }
+    
+    public double calcValorTotalPagar() {
+        for(Exame e : this.exames) {
+            this.valor += e.getValor();
+        }
+        
+        return this.valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    
     
 }
